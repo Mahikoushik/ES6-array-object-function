17fa2d3b797610f12caeabd867cc42d662c8e542
Mahikoushik <mahikoushik111@gmail.com>
2021-04-01 16:53:35 +0000

    Importing array, object and function using ES6 module

---
+++ importMultipleValues.mjs
@@ -0,0 +1,8 @@
+import { myArray, bulb, multiplyByFour } from "./exportMultipleValues.mjs";
+console.log(myArray);
+
+// import object from "./exportMultipleValues.mjs";
+console.log(bulb);
+
+// import funFour from "./exportMultipleValues.mjs";
+console.log(multiplyByFour(3));
